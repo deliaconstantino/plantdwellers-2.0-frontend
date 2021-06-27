961772b3cd20f637a2caa990431f11f627a09493
deliaconstantino <dconstan92@gmail.com>
2021-06-27 15:22:46 -0500

    altered login form to hold login fields in local state until submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,76 +1,78 @@
-import react from "react";
+import React from "react";
 import { connect } from "react-redux";
 import loginUserFetch from '../actions/loginUserFetch'
 
-const login = (props) => {
-  const handleSubmit = (event) => {
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        email: "",
+        password: ""
+    }
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
     console.log("in submit");
-    // console.log(props.formData)
-    props.loginUser(props.formData)
+    console.log(this.state)
+
+    this.props.loginUser(this.state)
   };
 
-  const handleChange = (event) => {
-    const updatedFormData = {
-      ...props.formData,
-      [event.target.name]: event.target.value,
-    };
-    console.log(updatedFormData)
-    props.updateLoginForm(updatedFormData)
+  handleChange = (event) => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    })
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="min-h-screen flex flex-col justify-top sm:py-12">
-        <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-          <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-            <div className="px-5 py-7">
-              <label className="font-semibold text-sm text-gray-600 pb-1 block">
-                E-mail
-              </label>
-              <input
-                type="text"
-                name="email"
-                value={props.formData.email}
-                onChange={handleChange}
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-              />
-              <label className="font-semibold text-sm text-gray-600 pb-1 block">
-                Password
-              </label>
-              <input
-                type="text"
-                name="password"
-                value={props.formData.password}
-                onChange={handleChange}
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-              />
-              <input
-                type="submit"
-                value="Login"
-                className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
-              />
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="min-h-screen flex flex-col justify-top sm:py-12">
+          <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+            <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+              <div className="px-5 py-7">
+                <label className="font-semibold text-sm text-gray-600 pb-1 block">
+                  E-mail
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                />
+                <label className="font-semibold text-sm text-gray-600 pb-1 block">
+                  Password
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                />
+                <input
+                  type="submit"
+                  value="Login"
+                  className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </form>
-  );
-};
-
-const mapStateToProps = (state) => {
-  return ({
-    formData: state.loginForm
-  })
-};
-
+      </form>
+    );
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
     updateLoginForm: (formData) => dispatch({ type: "UPDATE_LOGIN_FORM", formData }),
     loginUser: (formData) => dispatch(loginUserFetch(formData))
-    // loginUser: (formData) => dispatch({type: "LOGIN_USER", formData})
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(login);
+export default connect(null, mapDispatchToProps)(Login);
