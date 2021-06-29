efa0c5cd3fa4e9e5cd402cda2a66407023c7e8d0
deliaconstantino <dconstan92@gmail.com>
2021-06-29 17:35:55 -0400

    removed comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
 
   return (
     <div className="navbar flex flex-wrap items-center justify-between p-5 bg-green-200">
-      <img src="../../img/eco-construction-1894026_1280.png" alt="ACME" width="65" />
+      <img src="../../img/eco-construction-1894026_1280.png" alt="green house graphic" width="65" />
       <div className="flex md:hidden">
         <button id="hamburger">
           <img className="toggle block" src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png" width="40" height="40" />
