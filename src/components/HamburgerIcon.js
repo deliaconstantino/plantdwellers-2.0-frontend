fded48da7ed329afeb4c64750af949a92a565ce0
deliaconstantino <dconstan92@gmail.com>
2021-07-01 11:45:58 -0400

    moved Hamburger and X icons to independent components for NavBar

---
+++ src/components/HamburgerIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HamburgerIcon = () => {
+  return (
+    <img
+      className="toggle block"
+      src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png"
+      alt="hamburger"
+      width="40"
+      height="40"
+    />
+  );
+};
+
+export default HamburgerIcon;
