f4cae9fae03ba594dd00f0842d7c854eef791e5d
deliaconstantino <dconstan92@gmail.com>
2021-07-01 12:13:19 -0400

    changed colors to green shades in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,13 +17,11 @@ class NavBar extends React.Component {
   };
 
   render() {
+    const navLinkClass = "block md:inline-block text-green-900 hover:text-green-500 px-3 py-3 border-b-2 border-green-900 md:border-none"
+
     return (
-      <nav className="flex flex-wrap items-center justify-between p-5 bg-blue-200">
-        <img
-          src="http://acmelogos.com/images/logo-1.svg"
-          alt="ACME"
-          width="120"
-        />
+      <nav className="flex flex-wrap items-center justify-between p-5 bg-green-200">
+        <img src="../../img/eco-construction-1894026_1280.png" alt="green house" width="65" />
         {/* <!-- hamburger --> */}
         <div className="flex md:hidden">
           <button onClick={this.handleClick}>
@@ -34,40 +32,22 @@ class NavBar extends React.Component {
         <div
           className={
             this.state.clicked
-              ? "toggle md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none"
-              : "hidden md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none"
+              ? "toggle md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-green-900 md:border-none"
+              : "hidden md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-green-900 md:border-none"
           }
         >
-          <a
-            href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none"
-          >
+          <NavLink to="/plants" className={navLinkClass}>
+             Plants
+          </NavLink>
+          <NavLink to="/home" className={navLinkClass}>
             Home
-          </a>
-          <a
-            href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none"
-          >
-            Products
-          </a>
-          <a
-            href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none"
-          >
-            Pricing
-          </a>
-          <a
-            href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none"
-          >
-            Contact
-          </a>
-          <a
-            href="#"
-            className="block md:flex w-full md:w-auto px-4 py-2 text-right bg-blue-900 hover:bg-blue-500 text-white md:rounded"
-          >
-            Create Account
-          </a>
+          </NavLink>
+          <NavLink to="/profile" className={navLinkClass}>
+            Profile
+          </NavLink>
+          <NavLink to="/logout" className="block md:flex w-full md:w-auto px-4 py-2 text-right bg-green-900 hover:bg-green-500 text-white md:rounded">
+            Logout
+          </NavLink>
         </div>
         {/* <!-- cta --> */}
       </nav>
@@ -76,3 +56,18 @@ class NavBar extends React.Component {
 }
 
 export default NavBar;
+
+
+
+
+
+        // <NavLink to="/logout" className="toggle hidden md:flex w-full md:w-auto px-4 py-2 text-right bg-green-900 hover:bg-green-500 text-white md:rounded">
+        //   Logout
+        // </NavLink>
+
+      //   <a
+      //   href="#"
+      //   className="block md:flex w-full md:w-auto px-4 py-2 text-right bg-blue-900 hover:bg-blue-500 text-white md:rounded"
+      // >
+      //   Create Account
+      // </a>
