e5c6c4c883a0b4298776b88f8f8fa72bbd8ae62d
deliaconstantino <dconstan92@gmail.com>
2021-07-01 17:08:43 -0400

    incorporated React Calendar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+// function tileContent() {
+//   // Add class to tiles in month view only
+//   return <p>Sample</p>
+// }
+
+const onClickDay = (value, event) => {
+  // alert("Clicked day: " + value.toString());
+  console.log(value.toString())
+};
 
 const Home = () => {
-  return <div>Home component</div>;
+  const [value, setValue] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar
+        className={"bg-gray-300"}
+        onChange={setValue}
+        value={value}
+        // tileContent={({ date, view }) => <p>date</p>}
+        onClickDay={onClickDay}
+      />
+      ;
+    </div>
+  );
 };
 
 export default Home;
