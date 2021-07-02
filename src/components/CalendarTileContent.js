24ae6e3bbaff28d41933dec1c952c71995886a1c
deliaconstantino <dconstan92@gmail.com>
2021-07-02 16:56:43 -0400

    created CalendarTileContent component to render calendar day details

---
+++ src/components/CalendarTileContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalendarTileContent = props => {
+  console.log('tile', props)
+  return (
+    <div>
+      Tile Content
+    </div>
+  )
+}
+
+export default CalendarTileContent;
