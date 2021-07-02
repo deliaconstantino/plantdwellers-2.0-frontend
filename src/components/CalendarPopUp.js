98e4a81f684d75c5f2649a98f1ea5ac151008d46
deliaconstantino <dconstan92@gmail.com>
2021-07-02 17:31:25 -0400

    rendered plants and close button with callback prop

--- src/components/CalendarPopUp.js
+++ src/components/CalendarPopUp.js
@@ -5,6 +5,8 @@ const CalendarPopUp = props => {
   return (
     <div>Popup!
       <button>Date: {props.date}</button>
+      <button onClick={props.closePopUp}>close!</button>
+      {props.plants.map((plant, index) => <p>{plant}</p>)}
     </div>
   )
 }
