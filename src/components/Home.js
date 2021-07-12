a87a89c8d1c297e2de0407f82410852f875db6a8
deliaconstantino <dconstan92@gmail.com>
2021-07-12 16:20:27 -0400

    transferred fetch hook to HomeInfo and removed unused code from Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,14 @@
-import React, { useState, useEffect } from "react";
-// import addHomeInfo from "../actions/addHomeInfo";
-// import { connect } from "react-redux";
+import React from "react";
 import WateringScheduleCalendar from "./WateringScheduleCalendar";
 import HomeInfo from "./HomeInfo";
 
 const Home = (props) => {
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token"); //TODO: make this a constant?
-  //   fetch("http://localhost:3001/api/v1/homes", {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   })
-  //     .then((resp) => resp.json())
-  //     .then((resp) => {
-  //       console.log("homeFetch", resp);
-  //       const homeInfo = {
-  //         homeAttributes: resp.data,
-  //         users: resp.included,
-  //       };
-  //       console.log("myObject", homeInfo);
-  //       props.addHomeInfo(homeInfo);
-  //     });
-  // }, []);
-
   return (
     <div>
       <HomeInfo />
-      {/* {props.attributes && <HomeInfo attributes={props.attributes} users={props.users}/>} */}
       <WateringScheduleCalendar />
     </div>
   );
 };
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     addHomeInfo: (home) => dispatch(addHomeInfo(home)),
-//   };
-// };
-
-// const mapStateToProps = state => {
-//   return {
-//     attributes: state.home.attributes,
-//     users: state.home.users
-//   }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Home);
 export default Home;
