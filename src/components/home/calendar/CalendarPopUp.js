f96b08b0740b20b6d794f1f489cb0afd01ad9a8b
deliaconstantino <dconstan92@gmail.com>
2021-07-12 16:58:40 -0400

    updated handleChange to update state

--- src/components/home/calendar/CalendarPopUp.js
+++ src/components/home/calendar/CalendarPopUp.js
@@ -25,18 +25,23 @@ const CalendarPopUp = (props) => {
     console.log("in handle change")
     console.log("data", e.target.dataset.idx)
     console.log(e.target.checked)
-    const completedArray = completed;
+    const completedArray = [...completed];
     completedArray[e.target.dataset.idx] = e.target.checked;
     console.log('arr', completedArray)
     setCompleted(completedArray)
   }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('in submit', completed)
+  }
 
   return (
     <div>
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
         <button onClick={props.closePopUp}><XIcon /></button>
         <div className="md:flex">
+
           <div className="md:flex-shrink-0">
             <div className="uppercase block mt-1 text-lg leading-tight font-medium text-green-800 hover:underline">
               Date: {date}
@@ -44,8 +49,9 @@ const CalendarPopUp = (props) => {
 
             {/* <img className="h-48 w-full md:w-48" src="../../img/patrick-perkins-3wylDrjxH-E-unsplash.jpg" alt="Stock photo of home with plants"/> */}
           </div>
+
           <div className="p-8">
-            <form>
+            <form onSubmit={handleSubmit}>
               {props.plants.map((plant, index) => {
                 return (
                   // <p className="mt-2 text-gray-500" key={plant.plantId}>
@@ -55,9 +61,12 @@ const CalendarPopUp = (props) => {
                   <Checkbox key={plant.plantId} info={plant} idx={index} onChange={handleChange}/>
                 );
               })}
+              <input type="submit" value="save"/>
             </form>
           </div>
+
         </div>
+
       </div>
     </div>
   );
