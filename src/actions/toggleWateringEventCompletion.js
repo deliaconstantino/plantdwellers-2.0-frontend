564edfc4396da0e6f150b7a452435bb733c67675
deliaconstantino <dconstan92@gmail.com>
2021-07-12 21:24:39 -0400

    updated toggleWateringEventCompletion action to format params and send fetch request

--- src/actions/toggleWateringEventCompletion.js
+++ src/actions/toggleWateringEventCompletion.js
@@ -1,8 +1,26 @@
-const toggleWateringEventCompletion = completionData => {
-  debugger;
+const toggleWateringEventCompletion = data => {
+  // debugger;
   return (dispatch) => {
 
-    // fetch()
+    const token = localStorage.getItem("token")
+
+    const params = {
+      plant_event: data
+    }
+
+    const configObj = {
+      method: "PUT",
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+      body: JSON.stringify(params)
+    }
+
+    fetch(`http://localhost:3001/api/v1/plant_events/${data.eventId}`, configObj)
+      .then(resp => resp.json())
+      .then(response => console.log(response))
   }
 
 
