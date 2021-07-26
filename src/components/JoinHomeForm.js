3bb0d4260c918a4594877d4d4acae6ccb312d469
deliaconstantino <dconstan92@gmail.com>
2021-07-26 15:23:59 -0400

    add dispatch to backend to load all homes for dropdown

--- src/components/JoinHomeForm.js
+++ src/components/JoinHomeForm.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import loadAllHomes from "../actions/loadAllHomes"
 
 const JoinHomeForm = props => {
+
+  useEffect(() => {
+    const token = localStorage.getItem("token"); //TODO: make this a constant?
+    token && props.loadAllHomes(token);
+  }, []);
+
   return (
     <div> in join home form</div>
   )
 }
 
-export default JoinHomeForm;
+const mapDispatchToProps = dispatch => {
+  return ({
+    loadAllHomes: (token) => dispatch(loadAllHomes(token))
+  })
+}
+
+export default connect(null, mapDispatchToProps)(JoinHomeForm);
 
 //TODO: grab all existing homes from backend, render them in dropdown then send selection to backend to update user
