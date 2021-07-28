bb3ab64750f1d83baceadd4975751d3f228cbdb2
deliaconstantino <dconstan92@gmail.com>
2021-07-28 18:29:15 -0400

    add mapStateToProps to get currentUser and pass down to child

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import WateringScheduleCalendar from "./calendar/WateringScheduleCalendar";
 import HomeInfo from "./HomeInfo";
 
-const Home = (props) => {
+const Home = ({ currentUser }) => {
   return (
     <div>
-      <HomeInfo />
+      <HomeInfo currentUser={currentUser} />
       <WateringScheduleCalendar />
     </div>
   );
 };
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Home);
