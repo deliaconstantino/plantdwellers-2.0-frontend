7fa0df3a3039bdf960eb7154c36576e8c68a4e32
deliaconstantino <dconstan92@gmail.com>
2021-07-28 17:39:04 -0400

    used prettier for formatting

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,22 +1,21 @@
 const currentUser = (state = {}, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
-
-      let userHome = {}
+      let userHome = {};
       if (action.user.home) {
         userHome = {
           id: action.user.home.id,
           nickname: action.user.home.nickname,
           city: action.user.home.city,
           state: action.user.home.state,
-          country: action.user.home.country
-        }
+          country: action.user.home.country,
+        };
       }
 
       return {
         name: action.user.name,
         email: action.user.email,
-        home: userHome
+        home: userHome,
       };
     case "ADD_HOME_TO_CURRENT_USER":
       const homeObj = {
@@ -24,27 +23,27 @@ const currentUser = (state = {}, action) => {
         nickname: action.payload.attributes.nickname,
         city: action.payload.attributes.city,
         state: action.payload.attributes.state,
-        country: action.payload.attributes.country
-      }
+        country: action.payload.attributes.country,
+      };
 
       const newState = {
         ...state,
-        home: homeObj
-      }
+        home: homeObj,
+      };
       return {
         ...state,
-        home: homeObj
-      }
+        home: homeObj,
+      };
     case "CLEAR_CURRENT_USER":
       //TODO: this format or simply {}
       return {
         name: "",
         email: "",
-        home: {}
+        home: {},
       };
     default:
       return state;
   }
 };
 
-export default currentUser
+export default currentUser;
