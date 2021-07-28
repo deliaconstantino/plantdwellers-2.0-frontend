f2c2e92289b46ad0be58b0cfc01abf7d6dee78b8
deliaconstantino <dconstan92@gmail.com>
2021-07-28 17:43:38 -0400

    update formatting with prettier

--- src/actions/addUserToHome.js
+++ src/actions/addUserToHome.js
@@ -1,26 +1,23 @@
 const addUserToHome = (homeId) => {
-  const token = localStorage.getItem("token")
+  const token = localStorage.getItem("token");
   return (dispatch) => {
     const configObj = {
       method: "PUT",
       headers: {
-        //TODO: verify syntax below
-        "Authorization": `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
         "content-type": "application/json",
-        "accept": "application/json",
-      }
-    }
+        accept: "application/json",
+      },
+    };
     fetch(`http://localhost:3001/api/v1/homes/${homeId}`, configObj)
-    .then((resp) => resp.json())
-    .then((resp) => {
-      console.log('resp', resp)
-
-      dispatch({
-        type: "ADD_HOME_TO_CURRENT_USER",
-        payload: resp.data
-      })
-    });
-  }
-}
+      .then((resp) => resp.json())
+      .then((resp) => {
+        dispatch({
+          type: "ADD_HOME_TO_CURRENT_USER",
+          payload: resp.data,
+        });
+      });
+  };
+};
 
 export default addUserToHome;
