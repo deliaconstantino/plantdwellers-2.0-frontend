cdb9adc209c0a7cf7239ccdfc44f977fc899a121
deliaconstantino <dconstan92@gmail.com>
2021-07-28 13:47:26 -0400

    grabbed user data from redux state with connect and rendered in UserInfo component

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import CreateHomeForm from "./CreateHomeForm";
 
-const UserInfo = props => {
+const UserInfo = ({ user }) => {
+  console.log("user", user);
   return (
-    <div></div>
-  )
-}
+    <>
+      <div className="px-8 mb-4 text-center">
+        <h3 className="pt-4 mb-2 text-2xl">Welcome {user.name}</h3>
+        <p className="mb-4 text-sm text-gray-700">Name: {user.name}</p>
+        <p className="mb-4 text-sm text-gray-700">Email: {user.email}</p>
+        <p className="mb-4 text-sm text-gray-700">Home: {user.home.nickname}</p>
+      </div>
+    </>
+  );
+};
 
-export default UserInfo;
+const mapStateToProps = (state) => {
+  return {
+    user: state.currentUser,
+  };
+};
+
+export default connect(mapStateToProps)(UserInfo);
