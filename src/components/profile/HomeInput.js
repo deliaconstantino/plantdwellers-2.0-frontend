87a5b87306dcfc809c5f0683a0ac6cd39fb23773
deliaconstantino <dconstan92@gmail.com>
2021-07-28 17:47:39 -0400

    created capitalizedValue variable for use rendering input

--- src/components/profile/HomeInput.js
+++ src/components/profile/HomeInput.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-const HomeInput = ({ value }) => {
+const HomeInput = ({ value, currentState, onChange }) => {
+  const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
   return (
     <div className="py-2">
-      <label className="block mb-2 text-sm font-bold text-gray-700" for="email">
-        {value.charAt(0).toUpperCase() + value.slice(1)}
+      <label className="block mb-2 text-sm font-bold text-gray-700">
+        {capitalizedValue}
       </label>
       <input
         className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
         id={value}
         type="text"
         placeholder={`${value}...`}
+        name={capitalizedValue}
+        value={currentState}
+        onChange={onChange}
       />
     </div>
   )
