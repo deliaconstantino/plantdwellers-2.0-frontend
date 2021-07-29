1326953f56c38e2a11ca564f5a6a65889592f07c
deliaconstantino <dconstan92@gmail.com>
2021-07-29 12:16:10 -0400

    update Home to render Sidebar and CalendarContainer components

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-import WateringScheduleCalendar from "./calendar/WateringScheduleCalendar";
-import HomeInfo from "./HomeInfo";
+import HomeSidebar from "./HomeSidebar";
+import CalendarContainer from "./CalendarContainer";
 
 const Home = ({ currentUser }) => {
   return (
-    <div>
-      <HomeInfo currentUser={currentUser} />
-      <WateringScheduleCalendar />
-    </div>
+    <>
+      <div class="bg-white">
+        Home
+        <div class="grid grid-cols-12 gap-0 ">
+          <div class="col-span-12 sm:col-span-12 md:col-span-5 lg:col-span-4 xxl:col-span-4">
+            <HomeSidebar currentUser={currentUser} />
+          </div>
+          <div class="bg-fixed relative col-span-12 sm:col-span-12 md:col-span-7 lg:col-span-8 xxl:col-span-8 hidden md:block">
+            <CalendarContainer />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    currentUser: state.currentUser
-  }
-}
+    currentUser: state.currentUser,
+  };
+};
 
 export default connect(mapStateToProps)(Home);
