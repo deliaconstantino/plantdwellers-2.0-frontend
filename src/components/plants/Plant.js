c5cd2242f93cf7eccf124f437ca17ef573352fcf
deliaconstantino <dconstan92@gmail.com>
2021-07-30 14:02:31 -0400

    update wording on plant card and change backgroudn color to white

--- src/components/plants/Plant.js
+++ src/components/plants/Plant.js
@@ -8,19 +8,24 @@ const Plant = (props) => {
   };
 
   return (
-    <div className="p-8 border-box rounded-xl shadow-md max-w-sm flex flex-wrap">
+    <div className="p-8 border-box bg-white rounded-xl shadow-md max-w-sm flex flex-wrap">
+      <div>
+        <h3 className="pt-4 mb-2 text-2xl">{props.info.common_name}</h3>
+        <p className="p-2">Scientific Name: {props.info.scientific_name}</p>
+      </div>
       <div className="p-2">
         <img src="../../img/palm-4211167_1920.jpg" width="100" />
       </div>
       <div className="p-2">
-        <p className="p-2">{props.info.common_name}</p>
-        <p className="p-2">{props.info.scientific_name}</p>
-        <p className="p-2">{props.info.location}</p>
+        {/* <p className="p-2">{props.info.common_name}</p> */}
+
+        <p className="p-2">Location: {props.info.location}</p>
         <p className="p-2">
-          Watering rate: {props.info.watering_repeat_rate_days}
+          Water every {props.info.watering_repeat_rate_days} days
         </p>
         <button onClick={handleChange}>delete</button>
       </div>
+
     </div>
   );
 };
