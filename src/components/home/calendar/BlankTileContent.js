9aa3fa4134ace96e61e853b06ee859c5a7babb40
deliaconstantino <dconstan92@gmail.com>
2021-07-30 15:58:06 -0400

    render blank tile when no content

---
+++ src/components/home/calendar/BlankTileContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BlankTileContent = props => {
+  // const allComplete = (props.plants.find(plant => plant.completed === false)) ? false : true
+
+  return (
+    // <div className="p-10 px-10">
+    <div className="tileContent">
+      <p>{" "}</p>
+    </div>
+  )
+}
+
+export default BlankTileContent;
