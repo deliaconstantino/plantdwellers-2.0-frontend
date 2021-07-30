9c594b2b981eb7b8f86406f5effdea21072266e5
deliaconstantino <dconstan92@gmail.com>
2021-07-30 13:52:14 -0400

    import lime color scheme from Tailwind into project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      lime: colors.lime,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
