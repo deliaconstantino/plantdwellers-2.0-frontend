165d7628787a40dbad3912c95b0c13841f57ad69
Delia Constantino <dconstan92@gmail.com>
2021-08-09 12:31:58 -0400

    create NoHomeNotice component for a user who has not yet added a home to the app

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,23 +2,29 @@ import React from "react";
 import { connect } from "react-redux";
 import HomeSidebar from "./HomeSidebar";
 import CalendarContainer from "./CalendarContainer";
+import NoHomeNotice from "./NoHomeNotice";
 
 const Home = ({ currentUser }) => {
-  return (
-    <div className="md:container md:mx-auto font-mono p-2 justify-center">
-      <h3 className="uppercase block mt-1 text-6xl leading-tight font-medium text-lime-700 p-4">
-        {currentUser.home.nickname}
-      </h3>
-      <div className="flex flex-wrap space-x-6 space-y-4 items-start">
-        <div className="mt-2">
-          <HomeSidebar currentUser={currentUser} />
-        </div>
-        <div>
-          <CalendarContainer />
+  if (currentUser.home.nickname) {
+    return (
+      <div className="md:container md:mx-auto font-mono p-2 justify-center">
+        <h3 className="uppercase block mt-1 text-6xl leading-tight font-medium text-lime-700 p-4">
+          {currentUser.home.nickname}
+        </h3>
+        <div className="flex flex-wrap space-x-6 space-y-4 items-start">
+          <div className="mt-2">
+            <HomeSidebar currentUser={currentUser} />
+          </div>
+          <div>
+            <CalendarContainer />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <NoHomeNotice />
+  }
+
 };
 
 const mapStateToProps = (state) => {
---
+++ src/components/home/NoHomeNotice.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const NoHomeNotice = (props) => {
+  return (
+    <div className="font-mono">
+      <div className="container mx-auto">
+        <div className="flex justify-center align-center">
+          <div className="w-full lg:w-1/2 mt-10 p-5 border-box bg-white rounded-xl shadow-md rounded-lg">
+            <div className="px-8 mb-4 text-center">
+              <h3 className="pt-4 mb-2 text-2xl text-lime-800">
+                You haven't added your home yet. Click{" "}
+                <a className="underline" href="/profile">
+                  here
+                </a>{" "}
+                to update your home info!
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoHomeNotice;
