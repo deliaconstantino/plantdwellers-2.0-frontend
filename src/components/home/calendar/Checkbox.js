b5f35991f2553321f43e3200ed71bb25d7420723
Delia Constantino <dconstan92@gmail.com>
2021-08-09 12:48:48 -0400

    destructured props

--- src/components/home/calendar/Checkbox.js
+++ src/components/home/calendar/Checkbox.js
@@ -2,14 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import toggleWateringEventCompletion from "../../../actions/toggleWateringEventCompletion.js";
 
-const Checkbox = (props) => {
+const Checkbox = ({ toggleWateringEventCompletion, plantName, completed, id, name }) => {
   const handleChange = (e) => {
     const plant_event = {
       id: e.target.dataset.id,
       completed: e.target.checked,
     };
-
-    props.toggleWateringEventCompletion(plant_event);
+    toggleWateringEventCompletion(plant_event);
   };
 
   return (
@@ -17,13 +16,13 @@ const Checkbox = (props) => {
       <label className="mt-2 text-gray-500">
         <input
           type="checkbox"
-          name={props.plantName}
-          checked={props.completed}
+          name={plantName}
+          checked={completed}
           className="form-checkbox-input"
           onChange={handleChange}
-          data-id={props.id}
+          data-id={id}
         />
-        {` ${props.name}`}
+        {` ${name}`}
       </label>
     </div>
   );
