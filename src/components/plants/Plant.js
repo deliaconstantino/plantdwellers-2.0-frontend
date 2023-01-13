2950c29881f9efd031be04806c2d1f400ecb9a76
Delia Constantino <dconstan92@gmail.com>
2023-01-13 17:29:13 -0500

    update UI to display new data from OpenFarm api

--- src/components/plants/Plant.js
+++ src/components/plants/Plant.js
@@ -1,47 +1,56 @@
 import React from "react";
-import { connect } from "react-redux";
-import deletePlant from "../../actions/deletePlant";
+import { useDispatch } from "react-redux";
+import { deletePlant } from "../../actions/deletePlant";
+import { PlantDescription } from "./PlantDescription";
 
-const Plant = ({ deletePlant, info, id }) => {
-  const handleChange = (event) => {
-    deletePlant(id);
+export const Plant = ({ info, id }) => {
+  const dispatch = useDispatch();
+  console.log("info", info);
+
+  const handleClick = () => {
+    dispatch(deletePlant(id));
   };
 
   return (
-    <div className="p-8 border-box bg-white rounded-xl shadow-md flex flex-wrap">
-      <div>
-        <h3 className="pt-4 mb-2 text-3xl text-lime-800 font-bold">
-          {info.common_name}
-        </h3>
-        <p className="p-2">Scientific Name: {info.scientific_name}</p>
-      </div>
-      <div className="p-2">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      {info.image_url ? (
+        <img
+          className="w-full"
+          src={info.image_url}
+          alt="Sunset in the mountains"
+          height="300px"
+          width="auto"
+        />
+      ) : (
         <img
           src="../../img/palm-4211167_1920.jpg"
           width="100"
           alt="palm leaf"
         />
+      )}
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{info.common_name}</div>
+        <span className="inline-block bg-gray-200 rounded-full py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Scientific Name: {info.scientific_name}
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Location in home: {info.location}
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          Water every {info.watering_repeat_rate_days} day(s).
+        </span>
+        <div className="pt-4 pb-2">
+          <button
+            className="bg-lime-500 hover:bg-lime-700 text-white font-bold mt-2 py-2 px-4 rounded"
+            onClick={handleClick}
+          >
+            delete
+          </button>
+        </div>
       </div>
-      <div className="px-2">
-        <p className="px-2">Location: {info.location}</p>
-        <p className="px-2">
-          Water every {info.watering_repeat_rate_days} days
-        </p>
-        <button
-          className="bg-lime-500 hover:bg-lime-700 text-white font-bold mt-2 py-2 px-4 rounded"
-          onClick={handleChange}
-        >
-          delete
-        </button>
-      </div>
+      {info?.description && (
+        <PlantDescription description={info?.description} />
+      )}
     </div>
   );
 };
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    deletePlant: (id) => dispatch(deletePlant(id)),
-  };
-};
-
-export default connect(null, mapDispatchToProps)(Plant);
---
+++ src/components/plants/PlantDescription.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const ChevronDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+    />
+  </svg>
+);
+
+const ChevronUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="w-6 h-6"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+    />
+  </svg>
+);
+
+export const PlantDescription = ({ description }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const handleClick = () => {
+    setShowDescription(!showDescription);
+  };
+  return (
+    <div className="px-6">
+      See More
+      <button onClick={handleClick}>
+        {showDescription ? <ChevronUp /> : <ChevronDown />}
+      </button>
+      {showDescription && (
+        <p className="text-gray-700 text-base">{description}</p>
+      )}
+    </div>
+  );
+};
